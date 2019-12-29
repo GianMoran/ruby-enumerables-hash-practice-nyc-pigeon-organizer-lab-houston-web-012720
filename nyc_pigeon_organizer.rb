4c9ae65713933a1d3cd559433782a77d9fc597f1
Gian Moran <gpmoran_@outlook.com>
2019-12-29 05:48:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,13 +2,15 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 
 pigeon_list = {}
-binding.pry
+
 data.each do |keys,values| 
   binding.pry
   values.each do |attributes,names|
+    binding.pry
     names.each do |name|
       if pigeon_list[name] == nil
         pigeon_list[name] = {}
+        binding.pry
       end
       if pigeon_list[name][keys] == nil 
         pigeon_list[name][keys] = [attributes.to_s]
